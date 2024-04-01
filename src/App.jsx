dd816e980bbd9df211a37c649036a58ae759d9d3
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-04-01 11:54:40 +0200

    Add star animation to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { Stars } from '@react-three/drei';
+
+function StarAnimation() {
+  const starsRef = useRef();
+
+  useFrame(({ clock }) => {
+    starsRef.current.rotation.y = clock.getElapsedTime() * 0.1;
+  });
+
+  return (
+    <Stars
+      ref={starsRef}
+      radius={50}
+      depth={50}
+      count={10000}
+      factor={4}
+      saturation={0}
+      fade
+      speed={1}
+    />
+  );
+}
 
 function App() {
   return (
     <div className="h-screen bg-[#0b3d91]">
-      <Canvas style={{ height: "100vh", width: "100vw" }}>
-        <Stars />
+      <Canvas className="min-h-screen min-w-full">
+        <StarAnimation />
       </Canvas>
     </div>
   );
